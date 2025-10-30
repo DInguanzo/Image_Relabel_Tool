bb8f8da74aa40d06d7b527b9b7a0d5bf7645aab2
David Inguanzo <dki09@hotmail.com>
2025-10-29 22:41:26 -0500

    Added CLI input for tool

--- image_relabel.py
+++ image_relabel.py
@@ -1,6 +1,8 @@
 import exiftool # Tool for extracting image metadata
 import os # Interfacing with OS and file syste
 import re # Regular Expression 
+import argparse # Argument parser for CLI
+import pathlib
 
 """
 This script is supposed to rename image file names according to the date the image was taken using 
@@ -8,33 +10,52 @@ following format: YYYYMMDD_HHMMSS_event-name.ARW.
 
 Image date will be grabbed using the exifimage tool.
 """
+def renameImages(rootFilePath:pathlib.Path, eventName:str):
 
-eventName = "Ren-Fair"
-rootFilepath = r"/home/dinguanzo/Pictures/2025/10_Ren-Fair/RAW"
+    files = os.listdir(rootFilePath) # Gets a list of all files within root file path
+    pattern = r'(?P<Year>\d{4}):(?P<Month>\d{2}):(?P<Day>\d{2})\s+(?P<Hour>\d{2}):(?P<Minute>\d{2}):(?P<Second>\d{2})' # Pattern to extract info from metadata
 
-files = os.listdir(rootFilepath) # Gets a list of all files within root file path
-pattern = r'(?P<Year>\d{4}):(?P<Month>\d{2}):(?P<Day>\d{2})\s+(?P<Hour>\d{2}):(?P<Minute>\d{2}):(?P<Second>\d{2})' # Pattern to extract info from metadata
+    for file in files:
+        filepath = os.path.join(rootFilePath, file) # add file path to file name
 
-for file in files:
-    filepath = os.path.join(rootFilepath, file) # add file path to file name
+        with exiftool.ExifToolHelper() as et:
+            for d in et.get_tags(filepath, tags=["CreateDate"]): # Grab the date the image was created
 
-    with exiftool.ExifToolHelper() as et:
-        for d in et.get_tags(filepath, tags=["CreateDate"]): # Grab the date the image was created
+                for k, v in d.items(): # print out key-value pairs
 
-            for k, v in d.items(): # print out key-value pairs
+                    if (k == "EXIF:CreateDate"): # If key matches what we're looking for
 
-                if (k == "EXIF:CreateDate"): # If key matches what we're looking for
-
-                    match = re.search(pattern, v) # Extract info with regex
-                    if match:
-                        year = match[1]
-                        month = match[2]
-                        day = match[3]
-                        hour = match[4]
-                        minute = match[5]
-                        second = match[6]
-                        newFilename = year + month + day + "_" + hour + minute + second + "_" + eventName + ".ARW"
-                        os.rename(filepath, os.path.join(rootFilepath, newFilename)) # Rename file 
+                        match = re.search(pattern, v) # Extract info with regex
+                        if match:
+                            year = match[1]
+                            month = match[2]
+                            day = match[3]
+                            hour = match[4]
+                            minute = match[5]
+                            second = match[6]
+                            newFileName = year + month + day + "_" + hour + minute + second + "_" + eventName + ".ARW"
+                            os.rename(filepath, os.path.join(rootFilePath, newFileName)) # Rename file 
+    print("Done")
                 
+def sanitizeEventNames(eventName):
+    return (eventName.replace(" ", ""))
 
-print("Done")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='Image Relabel Tool',
+        description="Rename image file names according to the date  the image was taken using following format: YYYYMMDD_HHMMSS_event-name.ARW."
+    )
+    
+    parser.add_argument("filepath", 
+                        type=pathlib.Path, 
+                        help="The filepath to the folder that stores the images")
+    
+    parser.add_argument("eventName", 
+                        help="The event the photos are going to be named after")
+
+    args = parser.parse_args()
+
+    eventName = sanitizeEventNames(args.eventName)
+    
+    renameImages(args.filepath, eventName)
